96bab9e947fe1b077f7fb64e91af8155dff4bb03
AndresFelipe11 <andres.1701714898@gmail.com>
2023-05-02 00:17:51 -0500

    Changes views filteers en card Categorie Responsive

--- src/components/categories/headerResponsiveCategorie.tsx
+++ src/components/categories/headerResponsiveCategorie.tsx
@@ -194,6 +194,38 @@ const Icon: FC<PropsWithChildren> = ({children}) => <i>{children}</i>
               
             </ul>
               
+            <hr />
+            <div className="filterSelected">
+              <div className="containerMarca">
+              <a>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
+              </svg>
+              </a>
+              <p className='titleFilter'>Marca: </p> 
+              <p><strong>Marca Seleccionada</strong></p>
+              </div>
+              <div className="containerPrecio">
+              <a>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
+              </svg>
+              </a>
+              <p className='titleFilter'>Desde: </p> 
+              <p><strong>$5000</strong></p>
+              <p className='titleFilter'>Hasta: </p> 
+              <p><strong>$2000000</strong></p>
+              </div>
+              <div className="containerOrdenar">
+              <a>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
+              </svg>
+              </a>
+              <p className='titleFilter'>Ordenar: </p> 
+              <p><strong>El mas reciente</strong></p>
+              </div>
+            </div>
         </div>
                   
       </div>
