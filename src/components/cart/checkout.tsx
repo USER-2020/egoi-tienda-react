5ff244dd13d4d8af1ebe14c45a21c201c8c46efc
USER-2020 <hackingetico2021@gmail.com>
2023-05-26 00:01:06 -0500

    View Responsive, datos checkout

--- src/components/cart/checkout.tsx
+++ src/components/cart/checkout.tsx
@@ -684,13 +684,15 @@ function AddressCart() {
                         <p>Calle 10 # 20 - 20</p>
                       </div>
                       <div className="opciones">
-                      <a href="#">
+                      <a href="#" className='updateDireccion'>
                           <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
                           <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                           <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
                           </svg>
                         </a>
+                        <div className="checkPaymentResponsive">
                         <Input type='radio' style={{width:'30px', height:'30px', margin:'0'}}/>
+                        </div>
                       </div>
                   </li>
                   <li>
@@ -704,13 +706,15 @@ function AddressCart() {
                         <p>Calle 30 # 10 - 10</p>
                       </div>
                       <div className="opciones">
-                        <a href="#">
+                        <a href="#" className='updateDireccion'>
                           <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
                           <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                           <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
                           </svg>
                         </a>
+                        <div className="checkPaymentResponsive">
                         <Input type='radio' style={{width:'30px', height:'30px', margin:'0'}}/>
+                        </div>
                       </div>
                   </li>
                 </ul>
