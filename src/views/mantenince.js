942dbd43c4362290c4f41bc3842b84b49f224786
USER-2020 <hackingetico2021@gmail.com>
2023-08-26 12:24:55 -0500

    build: :ambulance: no work endpoints

---
+++ src/views/mantenince.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ManteniminetoPng from '../assets/png-transparent-web-development-web-page-web-design-website-maintenance-web-design-search-engine-optimization-web-hosting-service.png';
+
+const Mantenince = () => {
+  return (
+    <div>
+      <img src={ManteniminetoPng} width={'100%'} height={'100%'}/>
+    </div>
+  )
+}
+
+export default Mantenince
