b2312cb2c2166f9435536ccf0f1f4b5ccf6009f8
USER-2020 <hackingetico2021@gmail.com>
2023-09-02 16:05:11 -0500

    fix: email: userEmail reference in payment efecty

--- src/components/cart/checkout.tsx
+++ src/components/cart/checkout.tsx
@@ -977,7 +977,7 @@ function AddressCart() {
         transaction_amount: numericValue, // Monto total validado si es con cupón o no
         description: descriptionOrder, // Descripción concatenada de los productos del carrito de compras
         payment_method_id: "efecty", // Id del método de pago seleccionado
-        email: "juanfernandozuluaga2014310@gmail.com" // Email del usuario //userEmail
+        email: userEmail // Email del usuario //userEmail
       }
 
       generateEfectyREF(dataOrder, descriptionOrder);
