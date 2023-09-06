f553c701354af8770baf5c5b745275a856c73efc
USER-2020 <hackingetico2021@gmail.com>
2023-09-06 13:44:07 -0500

    build: remove costo de envio in detail cart

--- src/components/cart/detailCart.tsx
+++ src/components/cart/detailCart.tsx
@@ -413,7 +413,7 @@ function DetailCart({ setCantCart }) {
                       <th>Precio por unidad</th>
                       <th>Cantidad</th>
                       <th>Precio</th>
-                      <th>Costo de envío</th>
+                      {/* <th>Costo de envío</th> */}
                     </tr>
                   </thead>
                 </Table>
@@ -467,9 +467,9 @@ function DetailCart({ setCantCart }) {
                         </>
                       )}
                     </div>
-                    <div className="caracteristicaCostoEnvio">
+                    {/* <div className="caracteristicaCostoEnvio">
                       ${costoEnvio.toLocaleString('en')}
-                    </div>
+                    </div> */}
                     <a href="#" onClick={() => deleteOne(products.id)}>
                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
                         <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
