feacdd1dec5a760c7436c0d2c75d66bb3e0de831
USER-2020 <hackingetico2021@gmail.com>
2023-09-15 17:26:16 -0500

    fix: fix error carga de almodilla #

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -1,31 +1,41 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { getPreguntasFrecuentes } from '../services/sobreNosotros';
 import parse from 'html-react-parser';
 
 const PreguntasFrecuentes = () => {
+    const [data, setData] = useState([]);
 
-    const [data ,setData] = useState('');
-
-    const getFAQ = ()=>{
+    const getFAQ = () => {
         getPreguntasFrecuentes()
-        .then((res)=>{
-            console.log(res.data.data);
-            setData(res.data.data);
-        }).catch((err)=>console.log(err));
-    }
+            .then((res) => {
+                console.log(res.data);
+                setData(res.data);
+            })
+            .catch((err) => console.log(err));
+    };
 
-    useEffect(()=>{
+    useEffect(() => {
         getFAQ();
-    },[]);
+    }, []);
 
     return (
-        <div class="container for-container rtl" style={{ textAlign: 'left' }}>
-            <h2 class="text-center headerTitle">Preguntas frecuentes</h2>
-            <div class="for-padding">
-                {parse(data)}
+        <div className="container for-container rtl" style={{ textAlign: 'left' }}>
+            <h2 className="text-center headerTitle">Preguntas frecuentes</h2>
+            <div className="for-padding">
+                {data && data.length > 0 ? (
+                    // Mapea los datos para renderizar preguntas frecuentes
+                    data.map((dato, index) => (
+                        <div key={index}>
+                            {/* Renderiza el contenido de la pregunta utilizando parse */}
+                            {dato.question}- {dato.answer}
+                        </div>
+                    ))
+                ) : (
+                    <p>No hay preguntas registradas</p>
+                )}
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default PreguntasFrecuentes
+export default PreguntasFrecuentes;
