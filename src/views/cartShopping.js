825f985fd411414845101711429d2b04c0e269d1
USER-2020 <hackingetico2021@gmail.com>
2023-10-09 18:18:07 -0500

    perf: :zap: cambios estructurales y cmabio en el flujo de trabajo

--- src/views/cartShopping.js
+++ src/views/cartShopping.js
@@ -5,39 +5,54 @@ import Footer from '../components/footer'
 import React, { useEffect, useState, useRef } from 'react'
 import { allProductsCart } from "../services/cart";
 import { getCurrentUser } from './../helpers/Utils';
+import { getUserProfileInfo } from '../services/ordenes'
 
-  function CartShopping() {
+function CartShopping() {
 
-    const currenUser = getCurrentUser();
+  const currenUser = getCurrentUser();
 
-    const [cantProductsOnCart, setCantProductsOnCart] = useState('');
+  const [cantProductsOnCart, setCantProductsOnCart] = useState('');
 
-    const getCantCart = () => {
-      const token = currenUser.token;
-      allProductsCart(token)
-        .then((res) => {
-          const productsOncart = res.data;
-          // console.log("Respuesta de productos del carrito de compras desde el responsive", productsOncart);
-          const numberOfProducts = productsOncart.length;
-          // console.log("Cantidad de productos en el carrito desde el responsive", numberOfProducts);
-          setCantProductsOnCart(numberOfProducts);
+  //Info de perfil
+  const [detailInfoProfile, setDetailInfoProfile] = useState('');
 
 
-        }).catch((err) => console.log(err));
-    }
+  const getCantCart = () => {
+    const token = currenUser.token;
+    allProductsCart(token)
+      .then((res) => {
+        const productsOncart = res.data;
+        // console.log("Respuesta de productos del carrito de compras desde el responsive", productsOncart);
+        const numberOfProducts = productsOncart.length;
+        // console.log("Cantidad de productos en el carrito desde el responsive", numberOfProducts);
+        setCantProductsOnCart(numberOfProducts);
 
-    useEffect(()=>{
-      getCantCart();
-    },[]);
 
-    return (
-      <div className="w-100 d-flex flex-column align-items-center">
-        <Header cantCart={cantProductsOnCart}/>
-        <HeaderResponsive canCart={cantProductsOnCart}/>
-        <DetailCart setCantCart={()=>getCantCart()}/>
-        <Footer />
-      </div>
-    )
+      }).catch((err) => console.log(err));
   }
+
+  const getAllInfoPerfil = () => {
+    const token = currenUser.token;
+    getUserProfileInfo(token)
+      .then((res) => {
+        console.log("Info del cliente", res.data.f_name);
+        setDetailInfoProfile(res.data);
+      }).catch((err) => console.log(err));
+  }
+
+  useEffect(() => {
+    getCantCart();
+    getAllInfoPerfil();
+  }, []);
+
+  return (
+    <div className="w-100 d-flex flex-column align-items-center">
+      <Header cantCart={cantProductsOnCart} detailInfoProfile={detailInfoProfile}/>
+      <HeaderResponsive canCart={cantProductsOnCart} detailInfoProfile={detailInfoProfile}/>
+      <DetailCart setCantCart={() => getCantCart()} />
+      <Footer />
+    </div>
+  )
+}
 
 export default CartShopping
