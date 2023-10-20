59a20e79cdb32f993b0bf9e338355e91100df84c
USER - 2020 <hackingetico2021@gmail.com>
2023-10-20 03:36:43 -0500

    build: :art: changue position in btns detailProduct and detailCart

--- src/components/detailProduct/detailProduct.tsx
+++ src/components/detailProduct/detailProduct.tsx
@@ -1005,6 +1005,15 @@ function DetailProduct({ setCantCart, handleLogged }) {
                                 <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
                             </svg>
                         </div> */}
+
+                                            <div className="buyNowResponsive">
+                                                {detailProducts && detailProducts.current_stock && detailProducts.current_stock > 0 ? (
+
+                                                    <a href="#" onClick={(e) => { e.preventDefault(); buyNow() }} style={{ border: '1px solid #FC5241', borderRadius: '32px' }}>Comprar ahora</a>
+                                                ) : (
+                                                    <a href="#" style={{ pointerEvents: 'none', backgroundColor: 'gray', borderRadius: '32px', textDecoration: 'none', color: 'white', textAlign: 'center', justifyContent: 'center', fontWeight: 700 }}><p style={{ textAlign: 'center', marginBottom: '0' }}>Comprar ahora</p></a>
+                                                )}
+                                            </div>
                                             <div className="anadiralcarrito">
 
                                                 <a href="#" onClick={addToCart}>
@@ -1013,14 +1022,6 @@ function DetailProduct({ setCantCart, handleLogged }) {
                                                     </svg>
                                                     Añadir al carrito
                                                 </a>
-                                            </div>
-                                            <div className="buyNowResponsive">
-                                                {detailProducts && detailProducts.current_stock && detailProducts.current_stock > 0 ? (
-
-                                                    <a href="#" onClick={(e) => { e.preventDefault(); buyNow() }} style={{ border: '1px solid #FC5241', borderRadius: '32px' }}>Comprar ahora</a>
-                                                ) : (
-                                                    <a href="#" style={{ pointerEvents: 'none', backgroundColor: 'gray', borderRadius: '32px', textDecoration: 'none', color: 'white', textAlign: 'center', justifyContent: 'center', fontWeight: 700 }}><p style={{ textAlign: 'center', marginBottom: '0' }}>Comprar ahora</p></a>
-                                                )}
                                             </div>
 
                                         </div>
