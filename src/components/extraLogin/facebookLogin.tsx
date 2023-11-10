3ecc80fc5fcfe92970dd1882f6d8f6a01fc5da38
USER - 2020 <hackingetico2021@gmail.com>
2023-11-09 23:59:51 -0500

    fix: changues

--- src/components/extraLogin/facebookLogin.tsx
+++ src/components/extraLogin/facebookLogin.tsx
@@ -1,32 +1,26 @@
+import React from 'react';
 import FacebookLogin from '@greatsumini/react-facebook-login';
-import React from 'react'
-import { APP_ID_FACEBOOK } from '../../constants/defaultValues';
 
-
-const FacebookLoginComponent = () => {
-
+const FacebookLoginButton = () => {
     const responseFacebook = (response) => {
         if (response.status !== 'unknown') {
-            // El usuario ha iniciado sesión con éxito en Facebook
             console.log('Respuesta de Facebook:', response);
 
-            // Obtener más detalles del usuario desde la API de Facebook Graph
             fetch(`https://graph.facebook.com/v12.0/${response.id}?fields=id,first_name,last_name,email&access_token=${response.accessToken}`)
                 .then(res => res.json())
                 .then(data => {
                     console.log('Información adicional del usuario:', data);
                     const { first_name, last_name, email } = data;
-
-                    // Ahora puedes usar first_name y last_name según tus necesidades
+                    // Haz lo que necesites con la información del usuario
                 })
                 .catch(error => {
                     console.error('Error al obtener información del usuario desde la API de Facebook Graph:', error);
                 });
         } else {
-            // El usuario ha cancelado el inicio de sesión o ha ocurrido un problema
             console.log('Inicio de sesión cancelado o error');
         }
     };
+
     return (
         <FacebookLogin
             appId="TU_APP_ID_DE_FACEBOOK"
@@ -34,11 +28,26 @@ const FacebookLoginComponent = () => {
             fields="name,email,picture"
             callback={responseFacebook}
             render={renderProps => (
-                <button onClick={renderProps.onClick}>Iniciar sesión con Facebook</button>
+                <button
+                    onClick={renderProps.onClick}
+                    style={{
+                        backgroundColor: '#4267b2',
+                        color: '#fff',
+                        fontSize: '16px',
+                        padding: '12px 24px',
+                        border: 'none',
+                        borderRadius: '50px',
+                        width: '285px',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}
+                >
+                    Iniciar sesión con Facebook
+                </button>
             )}
         />
+    );
+};
 
-    )
-}
-
-export default FacebookLoginComponent;
+export default FacebookLoginButton;
