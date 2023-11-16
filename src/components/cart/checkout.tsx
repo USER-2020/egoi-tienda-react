da31872ef60ac5cd5dc825722f6e028191d08869
USER - 2020 <hackingetico2021@gmail.com>
2023-11-15 21:23:54 -0500

    fix: :bug: styles card validation

--- src/components/cart/checkout.tsx
+++ src/components/cart/checkout.tsx
@@ -734,7 +734,7 @@ function AddressCart({ offcanvasValidate }) {
 
   const closeModalUpdate = () => {
     setModalAddressUpdate(false);
-    window.location.reload();
+    // window.location.reload();
     // document.addEventListener('DOMContentLoaded', function () {
     //   // Obten el elemento con el ID específico
     //   var miDiv = document.getElementById('DireccionesCards');
@@ -2247,7 +2247,7 @@ function AddressCart({ offcanvasValidate }) {
         <ModalBody>
           {/* <ModalProcesandoPago /> */}
           <>
-            <div className="d-flex flex-column align-items-center gap-3">
+            <div className="d-flex flex-column align-items-center gap-3 text-center">
               <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="#FC5241" class="bi bi-exclamation-octagon" viewBox="0 0 16 16">
                 <path d="M4.54.146A.5.5 0 0 1 4.893 0h6.214a.5.5 0 0 1 .353.146l4.394 4.394a.5.5 0 0 1 .146.353v6.214a.5.5 0 0 1-.146.353l-4.394 4.394a.5.5 0 0 1-.353.146H4.893a.5.5 0 0 1-.353-.146L.146 11.46A.5.5 0 0 1 0 11.107V4.893a.5.5 0 0 1 .146-.353L4.54.146zM5.1 1 1 5.1v5.8L5.1 15h5.8l4.1-4.1V5.1L10.9 1H5.1z" />
                 <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z" />
